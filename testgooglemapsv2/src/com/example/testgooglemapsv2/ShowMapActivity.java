35e311d8fc9b3a884726c72e24de87867e715a12
karl <karl@fucking.xz>
2015-04-24 03:27:16 +0200

    test

---
+++ testgooglemapsv2/src/com/example/testgooglemapsv2/ShowMapActivity.java
@@ -0,0 +1,48 @@
+package com.example.testgooglemapsv2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class ShowMapActivity extends Activity {
+  static final LatLng PARIS = new LatLng(48.8566140, 2.3522219);
+  static final LatLng MARSEILLE = new LatLng(43.2964820, 5.3697800);
+  private GoogleMap map;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_show_map);
+    map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+        .getMap();
+    Marker hamburg = map.addMarker(new MarkerOptions().position(PARIS)
+        .title("Paris"));
+    Marker kiel = map.addMarker(new MarkerOptions()
+        .position(MARSEILLE)
+        .title("Doudou")
+        .snippet("Chat is cool")
+        .icon(BitmapDescriptorFactory
+            .fromResource(R.drawable.ic_launcher)));
+
+    // Move the camera instantly to hamburg with a zoom of 15.
+    map.moveCamera(CameraUpdateFactory.newLatLngZoom(PARIS, 15));
+
+    // Zoom in, animating the camera.
+    map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.show_map, menu);
+    return true;
+  }
+
+}
